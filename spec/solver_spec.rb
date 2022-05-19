4279bca29bda24b4ec9a4ed5973eed039d03f448
andres-condezo <ra.condezomonge@gmail.com>
2022-05-19 10:29:02 -0500

    Create the first test: test a new instance of solver class

---
+++ spec/solver_spec.rb
@@ -0,0 +1,10 @@
+require './solver'
+
+RSpec.describe Solver do
+  before :each do
+    @solver = Solver.new
+  end
+  it '@solver should be an instance of Solver' do
+    expect(@solver).to be_an_instance_of(Solver)
+  end
+end
